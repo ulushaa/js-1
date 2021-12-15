56d541a97c67cae1547c04c8998af4f9dfb2e225
ulusha <ummelkhatov@gmail.com>
2021-12-15 22:26:08 +0500

    js1

---
+++ script.js
@@ -0,0 +1,24 @@
+
+var str = prompt('Введите своё имя')
+console.log('Меня зовут '+str);
+
+var str = prompt('Введите свой возраст')
+console.log('Мне уже '+str);
+
+var str = prompt('Решите пример 6 + 17 =')
+console.log('Пример 1: 6 + 17 = 23 ваш ответ '+str);
+
+var str = prompt('Решите пример 4 * 5 =')
+console.log('Пример 2: 4 * 5 = 20 ваш ответ '+str);
+
+var str = prompt('Решите пример 7 * 8 =')
+console.log('Пример 3: 7 * 8 = 56 ваш ответ '+str);
+
+var str = prompt('Решите пример 60 / 4 =')
+console.log('Пример 4: 60 / 4 = 15 ваш ответ '+str);
+
+var str = prompt('Решите пример 7 + 12 =')
+console.log('Пример 5: 7 + 12 = 19 ваш ответ '+str);
+
+var str = prompt('Решите пример 15 % 4 =')
+console.log('Пример 6: 15 % 4 = 3 ваш ответ '+str);
